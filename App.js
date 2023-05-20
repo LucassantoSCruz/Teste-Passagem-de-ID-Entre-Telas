fcb929e9fc6078e28164d2fd959c83b78941b7bb
LucassantoSCruz <lucas.cruz162@etec.sp.gov.br>
2023-05-20 19:46:41 -0300

    Criação do App

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, TouchableOpacity, Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Stack = createStackNavigator();
+
+const MyComponent = ({ navigation }) => {
+  const data = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleItemPress(item.id)}>
+      <Text>{item.name}</Text>
+    </TouchableOpacity>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const handleItemPress = async (itemId) => {
+    try {
+      // Salve o ID do item no AsyncStorage
+      await AsyncStorage.setItem('selectedItemId', JSON.stringify(itemId));
+      // Navegue para a próxima tela
+      navigation.navigate('Detalhes');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+};
+
+const DetalhesScreen = () => {
+  const [itemId, setItemId] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchItemId = async () => {
+      try {
+        // Obtenha o ID do item armazenado no AsyncStorage
+        const storedItemId = await AsyncStorage.getItem('selectedItemId');
+        setItemId(JSON.parse(storedItemId));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchItemId();
+  }, []);
+
+  return (
+    <Text>Detalhes do item: {itemId}</Text>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyComponent} />
+        <Stack.Screen name="Detalhes" component={DetalhesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
